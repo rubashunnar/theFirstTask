5763b68fdc10fb6abaac7bd49cc96b853ac1e798
Ruba <shunnar.ruba@gmail.com>
2019-07-09 16:25:11 +0300

    21st Chapter: Tantrum

---
+++ 21stChapter/21st_task.ts
@@ -0,0 +1,75 @@
+import { AssertionError } from "assert";
+const fs=require("fs");
+const _=require("lodash")
+
+
+function extract_words(file_path){
+    let data;
+    
+    if (file_path==null||typeof(file_path)!=typeof("")){
+        throw new AssertionError();
+    }
+    try{
+        data=fs.readFileSync(file_path,'utf8')
+    }
+    catch(e){
+        console.log(e)
+        throw e;
+    }
+    return data.toLowerCase().replace(/[\W_]+/g, " ").split(" ");
+}
+
+function remove_stop_words(word_list){
+    let stop_words=[]
+    if (typeof(word_list)!=typeof([""])){
+        throw new AssertionError();
+    }
+    try{
+        stop_words=fs.readFileSync('stop-words.txt','utf8').split(",");
+    }
+    catch(e){
+        console.log(e)
+        throw e;
+    }
+    let words=[]
+    for (let i=0;i<word_list.length;i++){
+       if (stop_words.indexOf(word_list[i])==-1) words.push(word_list[i]) 
+    }
+    return words;
+}   
+
+function frequencies(word_list){
+    if (typeof(word_list)!=typeof([""])|| word_list==[]) throw new AssertionError();
+    let word_freq={};
+    for (let i=0;i<word_list.length;i++){
+        if (word_freq[word_list[i]]==undefined){
+            word_freq[word_list[i]]=1
+        }
+        else {
+            word_freq[word_list[i]]+=1
+        }
+    }
+    return word_freq
+}
+
+function sort(word_freq){
+    if (typeof(word_freq)!=typeof({})||word_freq=={})throw new AssertionError();
+    try{
+    return Object.entries(_.fromPairs(_.sortBy(_.toPairs(word_freq), 1).reverse()))
+    }
+    catch(e){
+        throw e
+    }
+}
+
+try{
+    let filename="in.txt"
+    let wf=sort(frequencies(remove_stop_words(extract_words(filename))))
+    if (typeof(wf)!= typeof([]))throw new AssertionError();
+    for (let i=0;i<25;i++){
+        console.log(wf[i]);
+    }
+}
+catch(e){
+    console.log(e.stack);
+}
---
+++ 21stChapter/word_indexQ2.ts
@@ -0,0 +1,86 @@
+import { AssertionError } from "assert";
+
+const fs=require("fs");
+const _=require("lodash");
+
+function extract_words(file_path){
+    let data;
+    if (file_path==null||typeof(file_path)!=typeof("")){
+        throw new AssertionError();
+    }
+    try{
+        data=fs.readFileSync(file_path,'utf8')
+    }
+    catch(e){
+        console.log(e)
+        throw e
+    }
+    return data.toLowerCase().replace(/[^a-z0-9\n]+/g, " ").split("\n");
+}
+
+function remove_stop_words(word_list){
+    let stop_words=[]
+    if (typeof(word_list)!=typeof([])){
+        throw new AssertionError();
+    }
+    try{
+        stop_words=fs.readFileSync('stop-words.txt','utf8').split(",");
+    }
+    catch(e){
+        console.log(e)
+        throw e
+    }
+    let words=[]
+    let line=[];
+    for (let i=0;i<word_list.length;i++){
+        let line_words=word_list[i].split(" ");
+        for (let m=0;m<line_words.length;m++){
+            if (stop_words.indexOf(line_words[m])==-1) line.push(line_words[m]) ;
+        }
+        words.push(line)
+        line=[]
+    }
+    return words;
+}   
+
+function frequencies(word_list){
+    if (typeof(word_list)!=typeof([""])|| word_list==[]) throw new AssertionError();
+    let word_freq={}
+    for (let i=0;i<word_list.length;i++){
+        let line=word_list[i];
+
+        for (let m=0;m<line.length;m++){
+            if (word_freq[line[m]]==undefined){
+                word_freq[line[m]]=[1,[Math.floor(i/45)+1]]
+            }
+            else {
+                    word_freq[line[m]][0]+=1
+                if (word_freq[line[m]][1].indexOf(Math.floor(i/45)+1)==-1){
+                    word_freq[line[m]][1].push(Math.floor(i/45)+1)
+                }
+            }
+        
+        }
+    }
+    return word_freq
+}
+
+function sort(word_freq){
+    if (typeof(word_freq)!=typeof({})||word_freq=={})throw new AssertionError();
+    return Object.entries(_.fromPairs(_.sortBy(_.toPairs(word_freq), 1).reverse()))
+}
+
+try{
+    let filename="in.txt"
+    //console.log(remove_stop_words(extract_words(filename)))
+    let wf=sort(frequencies(remove_stop_words(extract_words(filename))))
+    if (typeof(wf)!=typeof([]))throw new AssertionError();
+    for (let i=0;i<wf.length;i++){
+        if (wf[i][1][0]<100)
+            console.log(wf[i][0]+": "+wf[i][1][1]);
+    }   
+
+}
+catch(e){
+    console.log(e.stack)
+}
